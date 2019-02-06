54eeacbd9cd158561b615726b0c6d509a290b1df
Felix Kleine Boesing <felix.boesing@t-online.de>
2019-02-06 12:20:11 +0100

    update stone action space

--- src/Stone.py
+++ src/Stone.py
@@ -16,9 +16,8 @@ class Stone:
         self.value = value
         self.removed = False
 
-    def get_possible_moves(self, board_side: int, board: np.ndarray):
-        moves = {}
-        move = 0
+    def get_possible_moves(self, board_size: int, board: np.ndarray):
+        moves = []
         if abs(self.value) == 1:
             if self.start_row <= 2:
                 directions = [(1, -1), (1, 1)]
@@ -32,7 +31,7 @@ class Stone:
             while(within_board):
                 coord = self.coord + direction * i
                 value_board = board[coord[0], coord[1]]
-                within_board = self._check_if_position_on_board(coord, board_side)
+                within_board = self._check_if_position_on_board(coord, board_size)
                 # break if first step is already out of board
                 if not within_board:
                     break
@@ -41,15 +40,21 @@ class Stone:
                     break
                 # if there is no stone, than add this to move list.
                 if board[coord[0], coord[1]] == 0:
-                    moves[move] = {"new_coord": coord, "jumped_stones": [], "jumped_values": 0}
-                    move += 1
+                    moves += [{"new_coord": coord, "jumped_stones": [], "jumped_values": 0}]
                 # if there if a stone of the enemy
                 if value_board < 0 & self.value > 0 or value_board < 0 & self.value > 0:
                     # check if it can be jumped
                     coord_jump = coord + direction
-                    within_board_after_jump = self._check_if_position_on_board(coord_jump, board_side)
+                    within_board_after_jump = self._check_if_position_on_board(coord_jump, board_size)
+                    # break if place behind stone is out of border
                     if within_board_after_jump:
-                        moves[move] = {"new_coord": coord, "jumped_stones": [coord], "jumped_values": 0}
+                        break
+                    value_board_jump = board[coord[0], coord[1]]
+                    # break if there is no free place
+                    if value_board != 0:
+                        break
+                    moves_tmp = self.jump_enemy_stone(directions, board, coord_jump, board_size)
+                    moves += moves_tmp
 
                 i += 1
                 # break if normal stone, because they can only move one field
@@ -57,15 +62,38 @@ class Stone:
                     break
 
     def jump_enemy_stone(self, directions: list, board: np.ndarray, coord: tuple, board_size: int):
+        moves = []
         for direction in directions:
-            coord = coord + direction
-            if not self._check_if_position_on_board(coord, board_size=board_size):
-                continue
+            within_board = True
+            i = 1
+            while(within_board):
+                coord = coord + direction * i
+                within_board = self._check_if_position_on_board(coord, board_size)
+                # break if first step is already out of board
+                if not within_board:
+                    break
+                value_board = board[coord[0], coord[1]]
+                # break if there is a stone of them same player in the way
+                if value_board < 0 & self.value < 0 or value_board > 0 & self.value > 0:
+                    break
+                # if there if a stone of the enemy
+                if value_board < 0 & self.value > 0 or value_board < 0 & self.value > 0:
+                    # check if it can be jumped
+                    j = 1
+                    within_board_after_jump  = True
+                    while (within_board_after_jump):
+                        coord_jump = coord + direction * j
+                        within_board_after_jump = self._check_if_position_on_board(coord_jump, board_size)
+                        if not within_board_after_jump:
+                            break
+                        else:
+                            moves += self.jump_enemy_stone(directions, board, coord_jump, board_size)
+                            moves = [{"new_coord": coord, "jumped_stones": [coord], "jumped_values": 0}]
 
-
-
-
-
+                i += 1
+                # break if normal stone, because they can only move one field
+                if abs(self.value) == 1:
+                    break
 
     def move_stone(self):
         pass
