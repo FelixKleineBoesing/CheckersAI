f3034822280870999d16275c80d1ca5bf73eacb0
Felix Kleine Boesing <felix.boesing@t-online.de>
2019-02-07 17:34:55 +0100

    added testing of X Games and some evaluation if losses/wins

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,27 @@
 from src.Game import Game
 from src.Board import Board
 from src.Applicant import Applicant
-from src.applicants.PlayerOne import PlayerOne
-from src.applicants.PlayerTwo import PlayerTwo
+from src.applicants.RandomPlayerWithMaxValue import RandomPlayerWithMaxValue
 from src.applicants.RandomPlayer import RandomPlayer
 
 
 if __name__=="__main__":
     winners = []
-    for i in range(10):
+    for i in range(1000):
         board = Board(board_length=8)
-        player_one = RandomPlayer("One", "up")
+        player_one = RandomPlayerWithMaxValue("One", "up")
         player_two = RandomPlayer("Two", "down")
         game = Game("Test", player_one=player_one, player_two=player_two, board=board)
         game.play()
         winners += [game.winner]
+        print(winners)
+        victories_player_two = 0
+        victories_player_one = 0
+        for winner in winners:
+            if winner == "One":
+                victories_player_one += 1
+            if winner == "Two":
+                victories_player_two += 1
+
+    print(victories_player_one)
+    print(victories_player_two)
