725eddd593867a975d6d49aa69a784d7b7934cc1
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-05-11 12:06:09 +0200

    move ReplayBuffer

--- python_backend/src/agents/QLearningAgent.py
+++ python_backend/src/agents/QLearningAgent.py
@@ -1,18 +1,15 @@
 import numpy as np
 import tensorflow as tf
 import random
-import copy
 from keras.layers import Dense, Flatten
 import keras
-from math import log
 import os
-import matplotlib.pyplot as plt
-from pandas import DataFrame
 
 from python_backend.src.agents.Agent import Agent
 from python_backend.src.Helpers import ActionSpace
+from python_backend.src.ReplayBuffer import ReplayBuffer
 
-plt.interactive(True)
+
 class QLearningAgent(Agent):
 
     def __init__(self, state_shape: tuple, action_shape: tuple, name: str, side: str = "up", epsilon: float = 0.5,
@@ -175,9 +172,7 @@ class QLearningAgent(Agent):
         # define network
         with tf.variable_scope(self.name, reuse=False):
             network = keras.models.Sequential()
-
-            # let's create a network for approximate q-learning following guidelines above
-            # input shape: X x Y (board size)
+            # TODO try agent performance with rnn layers (perhaps LSTM since we have sequential dependencies in the data)
             network.add(Dense(512, activation="relu", input_shape=state_shape))
             network.add(Dense(1024, activation="relu"))
             network.add(Dense(2048, activation="relu"))
@@ -185,64 +180,3 @@ class QLearningAgent(Agent):
             network.add(Flatten())
             network.add(Dense(self.number_actions, activation="linear"))
         return network
-
-
-class ReplayBuffer(object):
-    def __init__(self, size):
-        """Create Replay buffer.
-        Parameters
-        ----------
-        size: int
-            Max number of transitions to store in the buffer. When the buffer
-            overflows the old memories are dropped.
-        """
-        self._storage = []
-        self._maxsize = size
-        self._next_idx = 0
-
-    def __len__(self):
-        return len(self._storage)
-
-    def add(self, obs_t, action, reward, obs_tp1, done):
-        data = (obs_t, action, reward, obs_tp1, done)
-
-        if self._next_idx >= len(self._storage):
-            self._storage.append(data)
-        else:
-            self._storage[self._next_idx] = data
-        self._next_idx = (self._next_idx + 1) % self._maxsize
-
-    def _encode_sample(self, idxes):
-        obses_t, actions, rewards, obses_tp1, dones = [], [], [], [], []
-        for i in idxes:
-            data = self._storage[i]
-            obs_t, action, reward, obs_tp1, done = data
-            obses_t.append(np.array(obs_t, copy=False))
-            actions.append(np.array(action, copy=False))
-            rewards.append(reward)
-            obses_tp1.append(np.array(obs_tp1, copy=False))
-            dones.append(done)
-        return np.array(obses_t), np.array(actions), np.array(rewards), np.array(obses_tp1), np.array(dones)
-
-    def sample(self, batch_size):
-        """Sample a batch of experiences.
-        Parameters
-        ----------
-        batch_size: int
-            How many transitions to sample.
-        Returns
-        -------
-        obs_batch: np.array
-            batch of observations
-        act_batch: np.array
-            batch of actions executed given obs_batch
-        rew_batch: np.array
-            rewards received as results of executing act_batch
-        next_obs_batch: np.array
-            next set of observations seen after executing act_batch
-        done_mask: np.array
-            done_mask[i] = 1 if executing act_batch[i] resulted in
-            the end of an episode and 0 otherwise.
-        """
-        idxes = [random.randint(0, len(self._storage) - 1) for _ in range(batch_size)]
-        return self._encode_sample(idxes)
