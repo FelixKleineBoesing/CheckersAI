11309cd0d6dee2add9538d520e360b1dcdf227b4
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-05-20 07:46:17 +0200

    some logging changes in gametester

--- checkers/tests/GameTester.py
+++ checkers/tests/GameTester.py
@@ -14,22 +14,23 @@ def run_random_vs_random_max():
     action_space = (board_length, board_length, board_length, board_length)
     agent_one = RandomAgentWithMaxValue((board_length, board_length), action_space, "One", "up")
     agent_two = RandomAgent((board_length, board_length), action_space, "Two", "down")
-    for i in range(1000):
+    iterations = 1000
+    for i in range(iterations):
         board = Board(board_length=8)
         game = Game("Test", agent_one=agent_one, agent_two=agent_two, board=board)
         game.play(verbose=False)
         winners += [game.winner]
+        if (i % 5000 == 0 and i > 0) or iterations - 1 == i:
+            victories_player_two = 0
+            victories_player_one = 0
+            for winner in winners:
+                if winner == "One":
+                    victories_player_one += 1
+                if winner == "Two":
+                    victories_player_two += 1
 
-    victories_player_two = 0
-    victories_player_one = 0
-    for winner in winners:
-        if winner == "One":
-            victories_player_one += 1
-        if winner == "Two":
-            victories_player_two += 1
-
-    print(victories_player_one)
-    print(victories_player_two)
+            logging.info("PLayer One: {}".format(str(victories_player_one)))
+            logging.info("PLayer One: {}".format(str(victories_player_two)))
 
 
 def run_random_vs_qlearning():
@@ -39,25 +40,24 @@ def run_random_vs_qlearning():
 
     agent_one = QLearningAgent((board_length, board_length), action_space, "qlearning", "up", 1.0, 2500, 50000)
     agent_two = RandomAgent((board_length, board_length), (board_length, board_length), "Two", "down")
-
-    for i in range(50000):
+    iterations = 50000
+    for i in range(iterations):
         board = Board(board_length=8)
         game = Game("Test", agent_one=agent_one, agent_two=agent_two, board=board)
         game.play(verbose=False)
         winners += [game.winner]
         agent_one.epsilon *= 0.9999
+        if (i % 5000 == 0 and i > 0) or iterations - 1 == i:
+            victories_player_two = 0
+            victories_player_one = 0
+            for winner in winners:
+                if winner == "qlearning":
+                    victories_player_one += 1
+                if winner == "Two":
+                    victories_player_two += 1
 
-    victories_player_two = 0
-    victories_player_one = 0
-
-    for winner in winners:
-        if winner == "qlearning":
-            victories_player_one += 1
-        if winner == "Two":
-            victories_player_two += 1
-
-    print(victories_player_one)
-    print(victories_player_two)
+            logging.info("PLayer One: {}".format(str(victories_player_one)))
+            logging.info("PLayer One: {}".format(str(victories_player_two)))
 
 
 def run_sarsa_vs_qlearning():
@@ -67,26 +67,24 @@ def run_sarsa_vs_qlearning():
 
     agent_one = QLearningAgent((board_length, board_length), action_space, "qlearning", "up", 0.0, 2000000000, 5000000000)
     agent_two = SARSAAgent((board_length, board_length), action_space, "sarsa", "down", 1.0, 2000, 50000)
-
-    for i in range(100):
+    iterations = 100
+    for i in range(iterations):
         board = Board(board_length=8)
         game = Game("Test", agent_one=agent_one, agent_two=agent_two, board=board)
         game.play(verbose=False)
         winners += [game.winner]
         agent_one.epsilon *= 0.9999
+        if (i % 5000 == 0 and i > 0) or iterations - 1 == i:
+            victories_player_two = 0
+            victories_player_one = 0
+            for winner in winners:
+                if winner == "qlearning":
+                    victories_player_one += 1
+                if winner == "sarsa":
+                    victories_player_two += 1
 
-    victories_player_two = 0
-    victories_player_one = 0
-
-    for winner in winners:
-        if winner == "One":
-            victories_player_one += 1
-        if winner == "Two":
-            victories_player_two += 1
-
-    print(victories_player_one)
-    print(victories_player_two)
-    print(victories_player_two)
+            logging.info("PLayer One: {}".format(str(victories_player_one)))
+            logging.info("PLayer One: {}".format(str(victories_player_two)))
 
 
 def run_sarsa_vs_random():
@@ -94,27 +92,26 @@ def run_sarsa_vs_random():
     board_length = 8
     action_space = (board_length, board_length, board_length, board_length)
 
-    agent_one = SARSAAgent((board_length, board_length), action_space, "sarsa", "up", 1.0, 2000, 50000)
+    agent_one = SARSAAgent((board_length, board_length), action_space, "sarsa", "up", 0.0, 2000, 50000)
     agent_two = RandomAgent((board_length, board_length), (board_length, board_length), "Two", "down")
-
-    for i in range(50000):
+    iterations = 100
+    for i in range(iterations):
         board = Board(board_length=8)
         game = Game("Test", agent_one=agent_one, agent_two=agent_two, board=board)
         game.play(verbose=False)
         winners += [game.winner]
         agent_one.epsilon *= 0.9999
+        if (i % 5000 == 0 and i > 0) or iterations - 1 == i:
+            victories_player_two = 0
+            victories_player_one = 0
+            for winner in winners:
+                if winner == "sarsa":
+                    victories_player_one += 1
+                if winner == "Two":
+                    victories_player_two += 1
 
-    victories_player_two = 0
-    victories_player_one = 0
-
-    for winner in winners:
-        if winner == "sarsa":
-            victories_player_one += 1
-        if winner == "Two":
-            victories_player_two += 1
-
-    logging.info("PLayer One: {}".format(str(victories_player_one)))
-    logging.info("PLayer One: {}".format(str(victories_player_two)))
+            logging.info("PLayer One: {}".format(str(victories_player_one)))
+            logging.info("PLayer One: {}".format(str(victories_player_two)))
 
 
 if __name__=="__main__":
