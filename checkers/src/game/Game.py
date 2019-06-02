77c961d12d9e2f2cb1087f61df367195e5b839dc
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-06-02 21:13:46 +0200

    make game play method more consistent

--- checkers/src/game/Game.py
+++ checkers/src/game/Game.py
@@ -59,77 +59,81 @@ class Game:
             reward_player_one, reward_player_two = 0, 0
             number_stones_before = self.board.number_of_stones()
             self.turns += 1
-
             if verbose:
                 print("Iteration:{}".format(self.turns))
             action_space_p_one = self.get_action_space(self.agent_one.name)
             # if player is blocked and can´t do any moves, than he has lost the game
-            if len(action_space_p_one) == 0:
+            if len(action_space_p_one) > 0:
+                action = self.agent_one.play_turn(self.board.board, action_space_p_one)
+                move, stone_id = self._get_move_and_stone(action, action_space_p_one)
+                rpo, rpt = self.board.move_stone(move, stone_id, self.rewards)
+                reward_player_one += self.rewards.turn + rpo
+                reward_player_two += rpt
+                state = self.board.board
+                self.board.refresh_board()
+                next_state = self.board.board
+                if self.save_runhistory:
+                    self.runhistory.append(next_state.tolist())
+                if verbose:
+                    print("--------Player One moved-----------")
+                    self.board.print_board()
+                finished, reason, winner = self._game_finished(turns_without_removed_stone)
+                if finished:
+                    if winner == self.agent_one.name:
+                        reward_player_one += self.rewards.win
+                        reward_player_two += self.rewards.loss
+                    else:
+                        reward_player_one += self.rewards.loss
+                        reward_player_two += self.rewards.win
+                self.agent_one.get_feedback(state, action, reward_player_one, next_state, finished)
+                self.agent_two.get_feedback(state, action, reward_player_two, next_state, finished)
+                cum_rewards_agent_one += reward_player_one
+                cum_rewards_agent_two += reward_player_two
+            else:
                 reason = "Player one is blocked and can´t move!"
                 winner = self.agent_two.name
-                break
-            action = self.agent_one.play_turn(self.board.board, action_space_p_one)
-            move, stone_id = self._get_move_and_stone(action, action_space_p_one)
-            rpo, rpt = self.board.move_stone(move, stone_id, self.rewards)
-            reward_player_one += self.rewards.turn + rpo
-            reward_player_two += rpt
-            state = self.board.board
-            self.board.refresh_board()
-            next_state = self.board.board
-            if self.save_runhistory:
-                self.runhistory.append(next_state.tolist())
-            if verbose:
-                print("--------Player One moved-----------")
-                self.board.print_board()
-            finished, reason, winner = self._game_finished(turns_without_removed_stone)
-            if finished:
-                if winner == self.agent_one.name:
-                    reward_player_one += self.rewards.win
-                    reward_player_two += self.rewards.loss
-                else:
-                    reward_player_one += self.rewards.loss
-                    reward_player_two += self.rewards.win
-            self.agent_one.get_feedback(state, action, reward_player_one, next_state, finished)
-            self.agent_two.get_feedback(state, action, reward_player_two, next_state, finished)
-            cum_rewards_agent_one += reward_player_one
-            cum_rewards_agent_two += reward_player_two
+                finished = True
             if finished:
                 break
 
             reward_player_one, reward_player_two = 0, 0
+            number_stones_before = self.board.number_of_stones()
+            self.turns += 1
             action_space_p_two = self.get_action_space(self.agent_two.name)
             # if player is blocked and can´t do any moves, than he has lost the game
-            if len(action_space_p_two) == 0:
+            if len(action_space_p_two) > 0:
+                action = self.agent_two.play_turn(self.board.board, action_space_p_two)
+                move, stone_id = self._get_move_and_stone(action, action_space_p_two)
+                rpt, rpo = self.board.move_stone(move, stone_id, self.rewards)
+                reward_player_two += self.rewards.turn + rpt
+                reward_player_one += rpo
+                state = self.board.board
+                self.board.refresh_board()
+                next_state = self.board.board
+                if self.save_runhistory:
+                    self.runhistory.append(next_state.tolist())
+                if verbose:
+                    print("--------Player two moved-----------")
+                    self.board.print_board()
+                number_stones_after = self.board.number_of_stones()
+                turns_without_removed_stone = turns_without_removed_stone + 1 if number_stones_after == \
+                                                                                 number_stones_before else 0
+                finished, reason, winner = self._game_finished(turns_without_removed_stone)
+                if finished:
+                    if winner == self.agent_two.name:
+                        reward_player_two += self.rewards.win
+                        reward_player_one += self.rewards.loss
+                    else:
+                        reward_player_one += self.rewards.loss
+                        reward_player_two += self.rewards.win
+                self.agent_two.get_feedback(state, action, reward_player_two, next_state, finished)
+                self.agent_one.get_feedback(state, action, reward_player_one, next_state, finished)
+                cum_rewards_agent_one += reward_player_one
+                cum_rewards_agent_two += reward_player_two
+            else:
                 reason = "Player two is blocked and can´t move!"
                 winner = self.agent_one.name
-                break
-            action = self.agent_two.play_turn(self.board.board, action_space_p_two)
-            move, stone_id = self._get_move_and_stone(action, action_space_p_two)
-            rpt, rpo = self.board.move_stone(move, stone_id, self.rewards)
-            reward_player_two += self.rewards.turn + rpt
-            reward_player_one += rpo
-            self.board.refresh_board()
-            state = self.board.board
-            if self.save_runhistory:
-                self.runhistory.append(next_state.tolist())
-            if verbose:
-                print("--------Player two moved-----------")
-                self.board.print_board()
-            number_stones_after = self.board.number_of_stones()
-            turns_without_removed_stone = turns_without_removed_stone + 1 if number_stones_after == \
-                                                                             number_stones_before else 0
-            finished, reason, winner = self._game_finished(turns_without_removed_stone)
-            if finished:
-                if winner == self.agent_two.name:
-                    reward_player_two += self.rewards.win
-                    reward_player_one += self.rewards.loss
-                else:
-                    reward_player_one += self.rewards.loss
-                    reward_player_two += self.rewards.win
-            self.agent_two.get_feedback(state, action, reward_player_two, next_state, finished)
-            self.agent_one.get_feedback(state, action, reward_player_one, next_state, finished)
-            cum_rewards_agent_one += reward_player_one
-            cum_rewards_agent_two += reward_player_two
+                finished = True
 
         if verbose:
             print("Game finished with the following message: {}".format(reason))
