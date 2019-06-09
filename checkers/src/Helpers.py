b7de0eda3a59dbb666402f37a5e3ca36cba55486
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-06-09 19:07:57 +0200

    added multiply and min_max_scaling helper functions

--- checkers/src/Helpers.py
+++ checkers/src/Helpers.py
@@ -104,3 +104,25 @@ class Config:
             except KeyError:
                 logging.error("Key {} is not present in config.json!")
 
+
+def multiply(*args):
+    """
+    Helper function which multiplies the all numbers that are delivered in the function call
+    :param args: numbers
+    :return: product of all numbers
+    """
+    product = 1
+    for arg in args:
+        product *= arg
+    return product
+
+
+def min_max_scaling(arr: np.ndarray, min_val: float=-2.0, max_val: float=2.0):
+    """
+    scales the given array between 0 and 1
+    :param arr: numpy array
+    :param min_val: min occurence in data, default is -2.0 based on the min possible stone value
+    :param max_val: max occurence in data, default is +2.0 based on the max possible stone value
+    :return: scaled numpy array
+    """
+    return (arr.astype('float32') - min_val) / (max_val - min_val)
